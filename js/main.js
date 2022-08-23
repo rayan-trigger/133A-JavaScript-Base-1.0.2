44522a18a35a33d8185c42096b242d4da733383e
Rondez Rayan <rayan.rondez@divtec.ch>
2022-08-23 08:40:46 +0200

    1st commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/**
+ * Fichier de base pour application web HTML/CSS/JS
+ * @author  Steve Fallet
+ * @version 0.1
+ * @since   2019-08-19
+ */
+
+(function main() {
+  'use strict'; // Demande une interprétation stricte du code
+
+    let a = 'Bonjour';
+    let b = 'toto';
+
+    console.log(a, b);
+}()); // Main IIFE
+
